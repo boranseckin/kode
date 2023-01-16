799119c39e33ca4eaf7ac45f038587f759ce2e23
Boran Seckin <boran@boranseckin.com>
2023-01-16 12:35:19 -0500

    Initial Commit

---
+++ Kode/KodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodeApp.swift
+//  Kode
+//
+//  Created by Boran Seckin on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct KodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
