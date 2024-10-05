fade62d4f4c95e2f81b71b21451333da3cb7dafa
Boran Seckin <boran@boranseckin.com>
2024-10-04 23:20:41 -0400

    feat: add 0 account view

--- Kode/ContentView.swift
+++ Kode/ContentView.swift
@@ -51,88 +51,92 @@ struct ContentView: View {
             } else {
                 NavigationStack {
                     VStack {
-                        ProgressBarView(progress: $progress)
-                            .padding(.init(top: 1, leading: 15, bottom: 0, trailing: 15))
-                            .onAppear {
-                                synced = false
-                            }
-                            .onReceive(timer) { time in
-                                if !synced {
-                                    progress = (30 - Double(Calendar.current.component(.second, from: time) % 30)) / 30
-                                    synced = true
-                                } else {
-                                    progress -= 0.01 / 3
-                                    if progress <= 0 {
-                                        progress = 1.0
-                                        synced = false
-                                    }
+                        if accountData.accounts.count == 0 {
+                            Text("Add a new account")
+                        } else {
+                            ProgressBarView(progress: $progress)
+                                .padding(.init(top: 1, leading: 15, bottom: 0, trailing: 15))
+                                .onAppear {
+                                    synced = false
                                 }
-                            }
-                        
-                        List {
-                            ForEach(accountData.accounts) { account in
-                                if (editMode == .active) {
-                                    AccountEditRowView(account: account)
-                                } else {
-                                    AccountRowView(account: account)
-                                        .padding(.vertical, 4)
-                                        .moveDisabled(true)
-                                        .deleteDisabled(true)
-                                        .onChange(of: synced, { oldValue, newValue in
-                                            accountData.updateCode(account: account)
-                                        })
-                                        .onAppear() {
-                                            accountData.updateCode(account: account)
+                                .onReceive(timer) { time in
+                                    if !synced {
+                                        progress = (30 - Double(Calendar.current.component(.second, from: time) % 30)) / 30
+                                        synced = true
+                                    } else {
+                                        progress -= 0.01 / 3
+                                        if progress <= 0 {
+                                            progress = 1.0
+                                            synced = false
                                         }
-                                }
-                            }
-                            .onMove(perform: accountData.move)
-                            .onDelete(perform: { index in
-                                toBeDeleted = index
-                                showDeleteAlert.toggle()
-                            })
-                            .alert(isPresented: $showDeleteAlert, content: {
-                                Alert(
-                                    title: Text("Are you sure you want to delete this account?"),
-                                    message: Text("This action is irreversable!"),
-                                    primaryButton: .destructive(Text("Yes")) {
-                                        accountData.remove(at: toBeDeleted!)
-                                        toBeDeleted = nil
-                                        showDeleteAlert = false
-                                    },
-                                    secondaryButton: .cancel() {
-                                        toBeDeleted = nil
-                                        showDeleteAlert = false
                                     }
-                                )
-                            })
-                        }
-                        .contentMargins(.top, 12, for: .scrollContent)
-                        .listSectionSpacing(0)
-                        .listStyle(.insetGrouped)
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading) {
-                                EditButton()
-                            }
-                            
-                            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                                Button {
-                                    showAdd.toggle()
-                                } label: {
-                                    Image(systemName: "plus")
-                                }.sheet(isPresented: $showAdd, content: {
-                                    AccountAddView()
-                                })
-                                
-                                NavigationLink(destination: SettingsView().navigationTitle("Settings")) {
-                                    Image(systemName: "gear")
                                 }
+                            
+                            List {
+                                ForEach(accountData.accounts) { account in
+                                    if (editMode == .active) {
+                                        AccountEditRowView(account: account)
+                                    } else {
+                                        AccountRowView(account: account)
+                                            .padding(.vertical, 4)
+                                            .moveDisabled(true)
+                                            .deleteDisabled(true)
+                                            .onChange(of: synced, { oldValue, newValue in
+                                                accountData.updateCode(account: account)
+                                            })
+                                            .onAppear() {
+                                                accountData.updateCode(account: account)
+                                            }
+                                    }
+                                }
+                                .onMove(perform: accountData.move)
+                                .onDelete(perform: { index in
+                                    toBeDeleted = index
+                                    showDeleteAlert.toggle()
+                                })
+                                .alert(isPresented: $showDeleteAlert, content: {
+                                    Alert(
+                                        title: Text("Are you sure you want to delete this account?"),
+                                        message: Text("This action is irreversable!"),
+                                        primaryButton: .destructive(Text("Yes")) {
+                                            accountData.remove(at: toBeDeleted!)
+                                            toBeDeleted = nil
+                                            showDeleteAlert = false
+                                        },
+                                        secondaryButton: .cancel() {
+                                            toBeDeleted = nil
+                                            showDeleteAlert = false
+                                        }
+                                    )
+                                })
+                            }
+                            .contentMargins(.top, 12, for: .scrollContent)
+                            .listSectionSpacing(0)
+                            .listStyle(.insetGrouped)
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            EditButton()
+                        }
+
+                        ToolbarItemGroup(placement: .navigationBarTrailing) {
+                            Button {
+                                showAdd.toggle()
+                            } label: {
+                                Image(systemName: "plus")
+                            }.sheet(isPresented: $showAdd, content: {
+                                AccountAddView()
+                            })
+                            
+                            NavigationLink(destination: SettingsView().navigationTitle("Settings")) {
+                                Image(systemName: "gear")
                             }
                         }
-                        .environment(\.editMode, $editMode)
-                        .navigationTitle("Kode")
-                        .navigationBarTitleDisplayMode(.inline)
                     }
+                    .environment(\.editMode, $editMode)
+                    .navigationTitle("Kode")
+                    .navigationBarTitleDisplayMode(.inline)
                 }
             }
         }
